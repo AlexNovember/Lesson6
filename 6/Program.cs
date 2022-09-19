5f102a6e334dc04d5af1181ccff542dcf0edd111
Alex <tanko77@rambler.ru>
2022-09-19 16:32:14 +0300

    1 zadanie

--- 6/Program.cs
+++ 6/Program.cs
@@ -1,2 +1,4 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Write("Введите элементы(через пробел): ");
+int[] arr = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+int count = arr.Count(x => x > 0);
+Console.WriteLine($"Кол-во элементов > 0: {count}");
